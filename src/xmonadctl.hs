e7480b0e899cd4cbbf60f141611397de4f9b10b4
tjnt <3m2dyzxu@gmail.com>
2021-09-11 20:21:27 +0900

    add xmonadctl for server-mode

---
+++ src/xmonadctl.hs
@@ -0,0 +1,22 @@
+import           Graphics.X11.Xlib        (allocaXEvent, clientMessage,
+                                           defaultScreen, internAtom,
+                                           openDisplay, rootWindow, sendEvent,
+                                           structureNotifyMask, sync)
+import           Graphics.X11.Xlib.Extras (currentTime, setClientMessageEvent,
+                                           setEventType)
+import           System.Environment       (getArgs)
+
+main :: IO ()
+main = getArgs >>= mapM_ (sendCommand "XMONAD_COMMAND")
+
+sendCommand :: String -> String -> IO ()
+sendCommand addr s = do
+  d  <- openDisplay ""
+  rw <- rootWindow d $ defaultScreen d
+  a <- internAtom d addr False
+  m <- internAtom d s False
+  allocaXEvent $ \e -> do
+                  setEventType e clientMessage
+                  setClientMessageEvent e rw a 32 m currentTime
+                  sendEvent d rw False structureNotifyMask e
+                  sync d False
