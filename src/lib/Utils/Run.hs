1b54036973b7b68680b63e87b62442c164f1d3ca
tjnt <3m2dyzxu@gmail.com>
2021-09-11 14:29:48 +0900

    add spawnWithOutput

--- src/lib/Utils/Run.hs
+++ src/lib/Utils/Run.hs
@@ -1,18 +1,23 @@
 module Utils.Run
-    ( spawnAndWait
+    ( spawnWithOutput
+    , spawnAndWait
     ) where
 
-import           Control.Monad  (when)
+import           Control.Monad  (void, when)
 import           System.IO      (hClose, hGetContents)
 import           System.Process (runInteractiveCommand)
 import           XMonad         (X, io)
 
-spawnAndWait :: String -> X ()
-spawnAndWait cmd = io $ do
+spawnWithOutput :: String -> X String
+spawnWithOutput cmd = io $ do
     (hin, hout, herr, _) <- runInteractiveCommand cmd
     out <- hGetContents hout
     when (out == out) $ return () -- wait for exit
     hClose hin >> hClose hout >> hClose herr
+    return out
+
+spawnAndWait :: String -> X ()
+spawnAndWait cmd = void $ spawnWithOutput cmd
 
 -- not work this
 -- spawnAndWait :: String -> X ()
