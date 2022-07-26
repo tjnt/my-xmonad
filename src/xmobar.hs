31d103bfa6cb92a2cbd0d39bd43068badc152d96
tjnt <3m2dyzxu@gmail.com>
2022-07-26 17:10:17 +0900

    xmobar: right click bluetoothicon launch bluetooth-tui

--- src/xmobar.hs
+++ src/xmobar.hs
@@ -56,7 +56,7 @@ config =
                                      . runTerminal "nmtui-connect" "" "3"
                 , "%bluetooth%" & (<> "%deviceicons%")
                                 . xmobarAction "xmonadctl bluetooth-toggle" "1"
-                                . xmobarAction "blueman-manager" "3"
+                                . runTerminal  "bluetooth-tui-exe" "" "3"
                 , "%dunst%" & xmobarAction "dunstctl history-pop" "1"
                             . xmobarAction "killall dunst ; dunst" "3"
                 , "%battery%"
