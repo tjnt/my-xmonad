b820aadef1f0733599c2257d79fcfdee539ee41d
tjnt <3m2dyzxu@gmail.com>
2022-08-02 00:49:43 +0900

    refactor

--- src/xmobar.hs
+++ src/xmobar.hs
@@ -23,138 +23,16 @@ import           Xmobar                        (Align (L), Command (Com),
                                                 defaultConfig, xmobar)
 import           Xmobar.Plugins.SimpleIOReader (SimpleIOReader (SimpleIOReader))
 
-config :: Config
-config =
-    defaultConfig
-        { font = myFont
-        , additionalFonts =
-            [ "xft:RictyDiminished Nerd Font:style=Regular:size=10"
-            , "xft:RictyDiminished Nerd Font:style=Regular:size=12"
-            , "xft:RictyDiminished Nerd Font:style=Regular:size=20"
-            ]
-        , bgColor = basebg
-        , fgColor = base07
-        , position = TopSize L 100 30
-        , lowerOnStart = True
-        , overrideRedirect = True
-        , persistent = True
-        , iconRoot = xmonadDir <> "/icons"
-        , sepChar = "%"
-        , alignSep = "}{"
-        , template =
-            xmobarFont 3 "\xe777"
-            <> " %UnsafeXMonadLog% }{"
-            <> concatMap (wrap " " " ")
-                [ "%cpu%" & runTerminal "htop -s PERCENT_CPU" "htop" "3"
-                , "%memory%" & runTerminal "htop -s PERCENT_MEM" "htop" "3"
-                , "%multicoretemp%"
-                , "%dynnetwork%" & runTerminal "nmtui-edit" "" "3"
-                , "%bright%" & xmobarAction "xmonadctl brightness-up" "4"
-                             . xmobarAction "xmonadctl brightness-down" "5"
-                , "%default:Master%" & xmobarAction "xmonadctl volume-master-toggle" "1"
-                                     . xmobarAction "xmonadctl volume-master-up" "4"
-                                     . xmobarAction "xmonadctl volume-master-down" "5"
-                                     . runTerminal "pulsemixer" "" "3"
-                , "%wifi%%wlp3s0wi%" & xmobarAction "xmonadctl wifi-toggle" "1"
-                                     . runTerminal "nmtui-connect" "" "3"
-                , "%bluetooth%" & (<> "%deviceicons%")
-                                . xmobarAction "xmonadctl bluetooth-toggle" "1"
-                                . runTerminal  "bluetooth-tui" "" "3"
-                , "%dunst%" & xmobarAction "dunstctl history-pop" "1"
-                            . xmobarAction "killall dunst ; dunst" "3"
-                , "%battery%"
-                , "%date%"
-                ]
-            <> "%trayerpad%"
-        , commands =
-            [ Run UnsafeXMonadLog
-            , Run $ Cpu
-                [ "--template", "<ipat><total>%"
-                , "--Low",      "40"
-                , "--High",     "85"
-                , "--normal",   base03
-                , "--high",     base01
-                , "--width",    "3"
-                , "--"
-                , "--load-icon-pattern", xmobarFont 1 "\xfb19"
-                ] 20
-            , Run $ Memory
-                [ "--template", "<usedipat><usedratio>%"
-                , "--Low",      "40"
-                , "--High",     "90"
-                , "--normal",   base03
-                , "--high",     base01
-                , "--width",    "3"
-                , "--"
-                , "--used-icon-pattern", xmobarFont 1 "\xf85a"
-                ] 20
-            , Run $ MultiCoreTemp
-                [ "--template", xmobarFont 1 "<avgbar>" <> "<avg>℃"
-                , "--bfore",    "\xf2cb\xf2cb\xf2ca\xf2ca\xf2c9\xf2c9\xf2c8\xf2c8\xf2c7\xf2c7"
-                , "--bwidth",   "0"
-                , "--Low",      "40"
-                , "--High",     "60"
-                , "--width",    "3"
-                -- , "--normal",   base03
-                -- , "--high",     base01
-                ] 40
-            , Run $ DynNetwork
-                [ "--template", "<rxipat><rx>kb  <txipat><tx>kb"
-                , "--Low",      "102400"
-                , "--High",     "1024000"
-                , "--normal",   base03
-                , "--high",     base01
-                , "--width",    "5"
-                , "--"
-                , "--rx-icon-pattern", xmobarFont 1 "\xf6d9"
-                , "--tx-icon-pattern", xmobarFont 1 "\xfa51"
-                ] 20
-            , Run $ Brightness
-                [ "--template", xmobarFont 1 "<bar>" <> "<percent>%"
-                , "--bfore",    "\xf5d9\xf5da\xf5db\xf5dc\xf5dd\xf5dd\xf5de\xf5de\xf5df\xf5df"
-                , "--bwidth",   "0"
-                , "--width",    "3"
-                , "--"
-                , "-D",         "intel_backlight"
-                ] 40
-            , Run $ Volume "default" "Master"
-                [ "--template", "<status><volume>%"
-                , "--bfore",    "\xfa7e\xfa7e\xfa7f\xfa7f\xfa7f\xfa7d\xfa7d\xfa7d\xf028\xf028"
-                , "--bwidth",   "0"
-                , "--width",    "3"
-                , "--"
-                , "--on",       xmobarFont 1 "<volumebar>"
-                , "--off",      xmobarFont 1 "\xfa80"
-                , "--onc",      base02
-                , "--offc",     base01
-                ] 40
-            , Run $ SimpleIOReader wifiIcon "wifi" 100
-            , Run $ Wireless "wlp3s0"
-                [ "--template", "<quality>%"
-                , "--width",    "3"
-                ] 40
-            , Run $ SimpleIOReader bluetoothIcon "bluetooth" 100
-            , Run $ SimpleIOReader deviceIcons "deviceicons" 100
-            , Run $ Battery
-                [ "--template", "<acstatus>"
-                , "--bfore",    "\xf244\xf243\xf243\xf243\xf242\xf242\xf242\xf241\xf241\xf240"
-                , "--bwidth",   "0"
-                , "--Low",      "20"
-                , "--High",     "80"
-                -- , "--low",      base01
-                -- , "--high",     base02
-                , "--"
-                , "-a",         "dunstify -a xmonad -u critical -h int:transient:1 'Battery running out !!'"
-                , "-A",         "10"
-                , "-o",         xmobarFont 1 "<leftbar> " <> "<left>% " <> "(<timeleft>) " <> "<watts>w"
-                , "-O",         xmobarFont 1 "<leftbar> " <> "<left>% " <> xmobarFont 1 "\xf0e7 " <> "<watts>w"
-                , "-i",         xmobarFont 1 "\xf1e6 " <> "<left>%"
-                ] 100
-            , Run $ SimpleIOReader dunstNotifyCount "dunst" 40
-            , Run $ Date "%m/%d %a %H:%M" "date" 100
-            , Run $ Com (xmonadDir <> "/scripts/trayer-padding-icon.sh") [] "trayerpad" 100
-            ]
-      }
+homeDir :: String
+homeDir = unsafeDupablePerformIO (getEnv "HOME")
+
+xmonadDir :: String
+xmonadDir = homeDir <> "/.xmonad"
+
+runTerminal :: String -> String -> String -> String -> String
+runTerminal cmd title =
+    xmobarAction $ "termite --exec " <> wrap "\"" "\"" cmd
+                 <> if null title then "" else " --title " <> title
 
 wifiIcon :: IO String
 wifiIcon = do
@@ -209,16 +87,143 @@ dunstNotifyCount = do
                    _   -> xmobarColor base02 "" "\xf868"
     return $ printf "%s %s" (xmobarFont 1 icon) stdout
 
-runTerminal :: String -> String -> String -> String -> String
-runTerminal cmd title =
-    xmobarAction $ "termite --exec " <> wrap "\"" "\"" cmd
-                 <> if null title then "" else " --title " <> title
+myAdditionalFonts :: [String]
+myAdditionalFonts =
+    [ "xft:RictyDiminished Nerd Font:style=Regular:size=10"
+    , "xft:RictyDiminished Nerd Font:style=Regular:size=12"
+    , "xft:RictyDiminished Nerd Font:style=Regular:size=20"
+    ]
 
-homeDir :: String
-homeDir = unsafeDupablePerformIO (getEnv "HOME")
+myTemplate :: String
+myTemplate =
+    xmobarFont 3 "\xe777"
+    <> " %UnsafeXMonadLog% }{"
+    <> concatMap (wrap " " " ")
+        [ "%cpu%"             & runTerminal "htop -s PERCENT_CPU" "htop" "3"
+        , "%memory%"          & runTerminal "htop -s PERCENT_MEM" "htop" "3"
+        , "%multicoretemp%"
+        , "%dynnetwork%"      & runTerminal "nmtui-edit" "" "3"
+        , "%bright%"          & xmobarAction "xmonadctl brightness-up" "4"
+                              . xmobarAction "xmonadctl brightness-down" "5"
+        , "%default:Master%"  & xmobarAction "xmonadctl volume-master-toggle" "1"
+                              . xmobarAction "xmonadctl volume-master-up" "4"
+                              . xmobarAction "xmonadctl volume-master-down" "5"
+                              . runTerminal "pulsemixer" "" "3"
+        , "%wifi%%wlp3s0wi%"  & xmobarAction "xmonadctl wifi-toggle" "1"
+                              . runTerminal "nmtui-connect" "" "3"
+        , "%bluetooth%"       & (<> "%deviceicons%")
+                              . xmobarAction "xmonadctl bluetooth-toggle" "1"
+                              . runTerminal  "bluetooth-tui" "" "3"
+        , "%dunst%"           & xmobarAction "dunstctl history-pop" "1"
+                              . xmobarAction "killall dunst ; dunst" "3"
+        , "%battery%"
+        , "%date%"
+        ]
+    <> "%trayerpad%"
 
-xmonadDir :: String
-xmonadDir = homeDir <> "/.xmonad"
+myCommands :: [Runnable]
+myCommands =
+    [ Run UnsafeXMonadLog
+    , Run $ Cpu
+        [ "--template", "<ipat><total>%"
+        , "--Low",      "40"
+        , "--High",     "85"
+        , "--normal",   base03
+        , "--high",     base01
+        , "--width",    "3"
+        , "--"
+        , "--load-icon-pattern", xmobarFont 1 "\xfb19"
+        ] 20
+    , Run $ Memory
+        [ "--template", "<usedipat><usedratio>%"
+        , "--Low",      "40"
+        , "--High",     "90"
+        , "--normal",   base03
+        , "--high",     base01
+        , "--width",    "3"
+        , "--"
+        , "--used-icon-pattern", xmobarFont 1 "\xf85a"
+        ] 20
+    , Run $ MultiCoreTemp
+        [ "--template", xmobarFont 1 "<avgbar>" <> "<avg>℃"
+        , "--bfore",    "\xf2cb\xf2cb\xf2ca\xf2ca\xf2c9\xf2c9\xf2c8\xf2c8\xf2c7\xf2c7"
+        , "--bwidth",   "0"
+        , "--Low",      "40"
+        , "--High",     "60"
+        , "--width",    "3"
+        -- , "--normal",   base03
+        -- , "--high",     base01
+        ] 40
+    , Run $ DynNetwork
+        [ "--template", "<rxipat><rx>kb  <txipat><tx>kb"
+        , "--Low",      "102400"
+        , "--High",     "1024000"
+        , "--normal",   base03
+        , "--high",     base01
+        , "--width",    "5"
+        , "--"
+        , "--rx-icon-pattern", xmobarFont 1 "\xf6d9"
+        , "--tx-icon-pattern", xmobarFont 1 "\xfa51"
+        ] 20
+    , Run $ Brightness
+        [ "--template", xmobarFont 1 "<bar>" <> "<percent>%"
+        , "--bfore",    "\xf5d9\xf5da\xf5db\xf5dc\xf5dd\xf5dd\xf5de\xf5de\xf5df\xf5df"
+        , "--bwidth",   "0"
+        , "--width",    "3"
+        , "--"
+        , "-D",         "intel_backlight"
+        ] 40
+    , Run $ Volume "default" "Master"
+        [ "--template", "<status><volume>%"
+        , "--bfore",    "\xfa7e\xfa7e\xfa7f\xfa7f\xfa7f\xfa7d\xfa7d\xfa7d\xf028\xf028"
+        , "--bwidth",   "0"
+        , "--width",    "3"
+        , "--"
+        , "--on",       xmobarFont 1 "<volumebar>"
+        , "--off",      xmobarFont 1 "\xfa80"
+        , "--onc",      base02
+        , "--offc",     base01
+        ] 40
+    , Run $ SimpleIOReader wifiIcon "wifi" 100
+    , Run $ Wireless "wlp3s0"
+        [ "--template", "<quality>%"
+        , "--width",    "3"
+        ] 40
+    , Run $ SimpleIOReader bluetoothIcon "bluetooth" 100
+    , Run $ SimpleIOReader deviceIcons "deviceicons" 100
+    , Run $ Battery
+        [ "--template", "<acstatus>"
+        , "--bfore",    "\xf244\xf243\xf243\xf243\xf242\xf242\xf242\xf241\xf241\xf240"
+        , "--bwidth",   "0"
+        , "--Low",      "20"
+        , "--High",     "80"
+        -- , "--low",      base01
+        -- , "--high",     base02
+        , "--"
+        , "-a",         "dunstify -a xmonad -u critical -h int:transient:1 'Battery running out !!'"
+        , "-A",         "10"
+        , "-o",         xmobarFont 1 "<leftbar> " <> "<left>% " <> "(<timeleft>) " <> "<watts>w"
+        , "-O",         xmobarFont 1 "<leftbar> " <> "<left>% " <> xmobarFont 1 "\xf0e7 " <> "<watts>w"
+        , "-i",         xmobarFont 1 "\xf1e6 " <> "<left>%"
+        ] 100
+    , Run $ SimpleIOReader dunstNotifyCount "dunst" 40
+    , Run $ Date "%m/%d %a %H:%M" "date" 100
+    , Run $ Com (xmonadDir <> "/scripts/trayer-padding-icon.sh") [] "trayerpad" 100
+    ]
 
 main :: IO ()
-main = xmobar config
+main = xmobar $ defaultConfig
+    { font = myFont
+    , additionalFonts = myAdditionalFonts
+    , bgColor = basebg
+    , fgColor = base07
+    , position = TopSize L 100 30
+    , lowerOnStart = True
+    , overrideRedirect = True
+    , persistent = True
+    , iconRoot = xmonadDir <> "/icons"
+    , sepChar = "%"
+    , alignSep = "}{"
+    , template = myTemplate
+    , commands = myCommands
+    }
