8cbc99036ed46f592500c46e5274bcc9a15fb520
tjnt <3m2dyzxu@gmail.com>
2022-09-17 00:50:01 +0900

    modify fzf path

--- scripts/clip.sh
+++ scripts/clip.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # used to select/delete clipboard history from xmonad.
-fzf=~/.zplug/bin/fzf
 export FZF_DEFAULT_OPTS='--layout=reverse --border --inline-info'
 
 case "$1" in
     "sel") 
-        recid=$(clipc --list | $fzf | awk '{print $1}')
+        recid=$(clipc --list | fzf | awk '{print $1}')
         [[ -n $recid ]] \
             && clipc --select $recid \
             && xclip -o -selection clipboard | xsel --clipboard
                     ;;
     "del")
-        recid=$(clipc --list | $fzf -m | awk '{print $1}')
+        recid=$(clipc --list | fzf -m | awk '{print $1}')
         for i in $recid; do clipc --delete $i; done
                     ;;
 esac
